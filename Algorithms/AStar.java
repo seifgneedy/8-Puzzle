6643959ec05fa10eb600f3e259b8b834d9821bd7
Ans Elnagar <ansgom3a@gmail.com>
2021-11-12 19:00:20 +0200

    Some modifications in A*

--- Algorithms/AStar.java
+++ Algorithms/AStar.java
@@ -1,5 +1,6 @@
 package Algorithms;
 
+import java.util.HashMap;
 import java.util.PriorityQueue;
 
 import Utils.Node;
@@ -8,7 +9,7 @@ import Utils.Node;
 
 public class AStar  extends Algorithm {
 	PriorityQueue<Node> frontier;
-
+	HashMap<Integer, Double> costs;
 	public static final int Manhattan = 0;
 	public static final int Euclidean = 1;
 	int heuristic;
@@ -17,12 +18,15 @@ public class AStar  extends Algorithm {
 		super();
 		this.heuristic = heuristic;
 		frontier=new PriorityQueue<Node>(10,new PriorityComparator());
+		costs = new HashMap<Integer, Double>();
 	}
 	@Override
 	boolean search(Node initialState, Result res) {
 		frontier.add(initialState);
+		costs.put(initialState.getState(), 0.0);
 		while(!frontier.isEmpty()){
 			Node state=frontier.poll();
+			costs.remove(state.getState());
 			if(explored.contains(state.getState())) {
 				continue;
 			}
@@ -40,6 +44,14 @@ public class AStar  extends Algorithm {
 						neighbor.setTotalcost(neighbor.getCost()+neighbor.manhattanCost());
 					}else if(heuristic == Euclidean) {
 						neighbor.setTotalcost(neighbor.getCost()+neighbor.euclideanCost());
+					}
+					if(costs.containsKey(neighbor.getState())) {
+						if(neighbor.getTotalcost() < costs.get(neighbor.getState())) {
+							costs.put(neighbor.getState(), neighbor.getTotalcost());
+							//frontier.remove(neighbor);
+						}else {
+							continue;
+						}
 					}
 					frontier.add(neighbor);
 				}
